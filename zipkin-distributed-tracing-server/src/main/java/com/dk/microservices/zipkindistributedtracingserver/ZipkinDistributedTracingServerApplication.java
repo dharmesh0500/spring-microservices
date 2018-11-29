d71056e5b744e3e22f4fac2ddf5ae23798264ae8
dharmesh0500 <dharmesh0500@gmail.com>
2018-11-30 00:35:00 +0530

    Adding microservices projects

---
+++ zipkin-distributed-tracing-server/src/main/java/com/dk/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -0,0 +1,14 @@
+package com.dk.microservices.zipkindistributedtracingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinDistributedTracingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedTracingServerApplication.class, args);
+	}
+}
